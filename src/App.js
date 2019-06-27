e450907896e7472c4ae1c2035fc6bb0afebac286
WXB <845225263@qq.com>
2019-06-28 00:02:09 +0800

    React开发简书
    React开发简书

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
+import { GlobalStyled } from './style.js' // 全局reset样式表
+import { IconFontGlobalStyled } from './statics/iconfont/iconfont' // 全局iconfont样式
+import Header from './common/header/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <GlobalStyled />
+      <IconFontGlobalStyled />
+      <Header />
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
